80f486817c9d00d39029387f37db52b03f44a9fd
bruno coelho <brunohsc51@gmail.com>
2017-08-31 09:56:46 -0300

    add devise confirmation email

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20170827171946) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "store_id"
-    t.integer  "user_id"
   end
 
   create_table "stocks", force: :cascade do |t|
@@ -65,8 +64,12 @@ ActiveRecord::Schema.define(version: 20170827171946) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "after creation" do
+		it "sends a confirmation email" do
+			user = FactoryGirl.build :user
+
+			expect {user.save}.to change(
+		 		Devise.mailer.deliveries, :count
+			).by(1)
+		end
+	end
 end
